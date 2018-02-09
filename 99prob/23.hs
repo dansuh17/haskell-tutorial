aac35b4f963da472a46262e59ba6751ee5a3ccb8
Dan Suh <kaehops@gmail.com>
2018-02-09 18:34:49 +0900

    23 add

--- 99prob/23.hs
+++ 99prob/23.hs
@@ -11,3 +11,14 @@ rnd_select 1 n
   | otherwise = do pos <- replicateM n $  -- unwrap IO Int -> Int
                             getStdRandom $ randomR (0, (length 1) - 1)
                    return [l!!p | p <- pos] -- wrap into IO
+
+rnd_select2 xs n = do
+  gen <- getStdGen
+  return $ take n [ xs !! x | x <- randomRs (0, (length xs) - 1) gen]
+
+
+rnd_select3 xs n
+  | n < 0 = error "N must be greater than zero"
+  | otherwise = replicateM n rand
+    where rand = do r <- randomRIO (0, (length xs) - 1)
+                    return (xs !! r)
